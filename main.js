ba2ab8f55976165bc25fad4b91015b667ac1ee1e
peppimakinen <111729175+peppimakinen@users.noreply.github.com>
2024-02-12 22:16:20 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import './style.css'
+
+import './style.css';
+
+const jokeContainer = document.querySelector('.show_joke');
+const chuckButton = document.querySelector('.chuck');
+
+chuckButton.addEventListener('click', fetchJoke);
+
+function fetchJoke() {
+  fetch('https://api.chucknorris.io/jokes/random')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Verkkovastaus ei ollut kunnossa');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      displayJoke(data.value);
+    })
+    .catch((error) => {
+      console.error('Fetch-operaatiossa ilmeni ongelma:', error);
+    });
+}
+
+function displayJoke(joke) {
+  jokeContainer.textContent = joke;
+}
